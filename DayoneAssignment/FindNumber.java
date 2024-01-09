ed9b7db9dcc6866b0a17536fda082ba4025f87ee
Archana-97104 <156064407+Archana-97104@users.noreply.github.com>
2024-01-09 13:48:37 +0530

    Add files via upload
    Day-1

---
+++ DayoneAssignment/FindNumber.java
@@ -0,0 +1,15 @@
+package DayoneAssignment;
+
+public class FindNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+          int num = 45;
+          if(num>=0) {
+        	  System.out.println("num is positive");
+          }else {
+        	  System.out.println("num is negative");
+          }
+	}
+
+}
---
+++ DayoneAssignment/FireFox.java
@@ -0,0 +1,18 @@
+package DayoneAssignment;
+
+public class FireFox {
+   public static void main (String[]args) {
+	   int num = 4;
+	   float browserversion = 16.67f;
+	   String browsername = "Chrome";
+boolean pageloaded = true;
+	   char browserlogo = 'g';
+	   System.out.println (browserversion);
+	   System.out.println(browsername);
+	   System.out.println(pageloaded);
+	   
+	   System.out.println(browserlogo);
+	   System.out.println(num);
+	   
+   }
+}
